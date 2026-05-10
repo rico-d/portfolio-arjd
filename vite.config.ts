95afdca5f480d602670e53c2dfadde5006d3dcb6
rico-d <rico.desalisa@yahoo.com>
2026-05-10 19:00:40 +0800

    add config changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          'mui-vendor': ['@mui/material', '@mui/icons-material', '@emotion/react', '@emotion/styled'],
+          'redux': ['@reduxjs/toolkit', 'react-redux'],
+        },
+      },
+    },
+  },
   server: {
     proxy: {
       '/api': {
